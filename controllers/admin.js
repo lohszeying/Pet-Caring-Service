cffc17507695f74bc3e27f7834eba48fbf0d1209
Huiren Woo <giantcrabby@gmail.com>
2020-10-26 17:04:36 +0800

    Add initial admin controller

---
+++ controllers/admin.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function (req, res) {
+});
+
+router.get('/login', function (req, res) {
+})
+
+module.exports = router
