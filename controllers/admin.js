98c03035d28b7b833d8345eda4f96f563012e42e
Huiren Woo <giantcrabby@gmail.com>
2020-10-26 22:56:47 +0800

    Add admin login

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
 
 router.get('/', function (req, res) {
 });
 
 router.get('/login', function (req, res) {
-    res.render('admin/login');
+    res.render('admin/login', {error: null});
 });
 
-router.post('/login', function (req, res) {
-    res.render('admin/login');
+router.post('/login', function (req, res, next) {
+    passport.authenticate('admin', (err, admin, info) => {
+        const error = err || info;
+        if (error) {
+            return res.render('admin/login', {error: error});
+        }
+
+        if (admin) {
+            req.logIn(admin, function (err) {
+                if (err) {
+                    return res.render('admin/login', {error: err});
+                }
+
+                return res.redirect('/admin/dashboard');
+            });
+        }
+
+        return res.render('admin/login', {error: "Incorrect login details!"});
+    })(req, res, next);
 });
 
 module.exports = router
