d32a1ad839aefe7123fcc3b9aadc40b0edeef24c
Park Junhyuk <ysy04136@gmail.com>
2020-10-28 04:11:04 +0800

    bids.js and search avail

--- controllers/bids.js
+++ controllers/bids.js
@@ -1,8 +1,15 @@
+require('dotenv').load();
 const express = require('express');
 const router = express.Router();
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+const sql_query = require('../sql');
 const passport = require('passport');
 
-router.get('/search-availability', passport.authMiddleware(), function (req, res) {
+router.get('/search-availability', passport.authMiddleware(), function (req, res,next) {
+   
     const info = {
         page: 'bids/search-availability',
         user: req.user.username,
@@ -10,10 +17,22 @@ router.get('/search-availability', passport.authMiddleware(), function (req, res
         area: req.user.area,
         enabled: req.user.enabled,
         auth: true,
-        caretakers: []
+        caretakers: [],
+        pet_tbl:[],
+        
     };
 
-    res.render("bids/search-availability", info);
+    pool.query(sql_query.query.all_pet_types, (err, data) => {
+        if (err) {
+           console.error(err);
+        } else {
+            info.pet_tbl = data.rows;
+        }
+        
+        res.render("bids/search-availability", info);
+    });
+    
+  
 });
 
 module.exports = router
