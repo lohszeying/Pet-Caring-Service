caaf9c0178ae4383f22a7fa7d56082d24c781fea
pooty3 <aloysiuslim.dw@gmail.com>
2020-10-28 18:50:46 +0800

    Create triggers to do check before allowing caretaker availability to be deleted (e.g. applying for leave)

--- sql/create.sql
+++ sql/create.sql
@@ -105,6 +105,7 @@ DO $$
 $$;
 
 --some test entries to bids
+DELETE FROM CARETAKERAVAILABILITY WHERE username = '400' AND date = '2020-09-09';
 INSERT INTO CARETAKERAVAILABILITY(username, date) VALUES ('401', '2020-08-09');
 INSERT INTO CARETAKERAVAILABILITY(username, date) VALUES ('401', '2020-08-08');
 INSERT INTO Bids(owner_username, pet_name, caretaker_username, start_date, end_date) VALUES ('6','4','400','2020-08-09', '2020-08-30');
@@ -115,6 +116,8 @@ INSERT INTO Bids(owner_username, pet_name, caretaker_username, start_date, end_d
 INSERT INTO Bids(owner_username, pet_name, caretaker_username, start_date, end_date) VALUES ('2','5','401','2020-08-09', '2020-08-09');
 INSERT INTO Bids(owner_username, pet_name, caretaker_username, start_date, end_date) VALUES ('3','5','401','2020-08-09', '2020-08-09');
 INSERT INTO Bids(owner_username, pet_name, caretaker_username, start_date, end_date) VALUES ('4','5','401','2020-08-09', '2020-08-09');
+DELETE FROM CARETAKERAVAILABILITY WHERE username = '401' AND date = '2020-08-09';
+--suppose to return constrain error
 UPDATE Bids SET status = 'ACCEPTED' WHERE owner_username = '1';
 
 SELECT * FROM BIDS;
