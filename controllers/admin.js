b25a7b78f012f775554b56dc599f198706d22406
Huiren Woo <giantcrabby@gmail.com>
2020-10-29 15:11:41 +0800

    Add pricing adjustment for pet type

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,4 +65,47 @@ router.post('/caretaker-stats', passport.authMiddleware(), (req, res) => {
     });
 });
 
+router.get('/pricing', passport.authMiddleware(), (req, res) => {
+    const info = {
+        user: req.user.username,
+        auth: true,
+        petTypePricing: null
+    };
+
+    pool.query(sql_query.admin.get_pet_type_pricing, (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            info.petTypePricing = data.rows;
+        }
+
+        res.render('admin/pricing', info);
+    });
+
+});
+
+router.post('/pricing', passport.authMiddleware(), (req, res) => {
+    const info = {
+        user: req.user.username,
+        auth: true,
+        petTypePricing: null
+    };
+
+    pool.query(sql_query.admin.update_pet_type_pricing, [req.body.pet_type, req.body.base_price], (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+
+        pool.query(sql_query.admin.get_pet_type_pricing, (err2, data2) => {
+            if (err2) {
+                console.error(err2);
+            } else {
+                info.petTypePricing = data2.rows;
+            }
+
+            res.render('admin/pricing', info);
+        });
+    });
+});
+
 module.exports = router
